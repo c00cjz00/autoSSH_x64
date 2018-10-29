de553703992f4d60838396db6e44be7bf78130f1
c00cjz00 <summerhill001@gmail.com>
2018-10-29 11:59:28 +0800

    init

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@ git config --global user.name "c00cjz00"
 git config --global user.email summerhill001@gmail.com
 #git pull
 git checkout master
-git rm crontab-crontab.php crontab-sftp.php
+git rm crontab-crontab.php crontab-sftp.php run_Filezilla.bat autoFileZilla.php 
+git rm -r FileZillaPortable
 git add *
 git commit -m "init"
 # 上傳至遠端
