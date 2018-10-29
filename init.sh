9215a0f3836135b28ac39388d6ef38d24942c6c8
c00cjz00 <summerhill001@gmail.com>
2018-10-29 12:02:23 +0800

    init

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ git config --global user.name "c00cjz00"
 git config --global user.email summerhill001@gmail.com
 #git pull
 git checkout master
-git rm init.sh~ config.php~ README.md~
-git rm -r FileZillaPortable
+#git rm init.sh~ config.php~ README.md~
+#git rm -r FileZillaPortable
 git add *
 git commit -m "init"
 # 上傳至遠端
